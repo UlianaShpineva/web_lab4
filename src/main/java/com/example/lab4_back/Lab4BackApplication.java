6854af8ef9bf81ddc363e8b240287c138fa7a400
Uliana <uliana.shpineva@gmail.com>
2024-03-17 18:07:28 +0300

    .

---
+++ src/main/java/com/example/lab4_back/Lab4BackApplication.java
@@ -0,0 +1,14 @@
+package com.example.lab4_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class Lab4BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4BackApplication.class, args);
+    }
+
+}
